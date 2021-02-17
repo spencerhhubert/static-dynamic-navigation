81b8596699a038274995c8a14745f118b8c75117
spencerhhubert <spencerhhubert@gmail.com>
2021-02-17 05:28:20 -0800

    made it pritty and title support

--- header.js
+++ header.js
@@ -1,4 +1,7 @@
 let nav = document.getElementById("nav");
+let head = document.getElementsByTagName("head")[0];
+
+head.innerHTML = '<link rel="stylesheet" href="style.css">'
 
 let pages = [
     {
@@ -24,9 +27,12 @@ for (i = 0; i < pages.length; i++) {
 
     if (pages[i].path == livePage) {
         link.classList.add("active");
+
+        title = document.createElement("title");
+        title.innerHTML = pages[i].name;
+        head.appendChild(title)
     }
     link.setAttribute("href", `${pages[i].path}`)
     link.innerHTML = `${pages[i].name}`;
     nav.appendChild(link);
 }
-
